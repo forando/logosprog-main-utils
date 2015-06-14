4f8ce88e797944166d7128fb1a048cedb4adfd0d
ANDREY LOGOSHKO <forando@users.noreply.github.com>
2015-06-14 14:59:18 +0300

    PropertiesBuilder interface created

---
+++ src/properties/builders/PropertiesBuilder.java
@@ -0,0 +1,12 @@
+package properties.builders;
+
+import java.util.Properties;
+
+/**
+ * Created by forando on 14.06.15.<br>
+ * Implement this interface to add factory method that
+ * builds Properties object
+ */
+public interface PropertiesBuilder {
+    Properties build();
+}

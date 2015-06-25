f786688e5bb00e9cfa1dee8f9c416a3a70a275da
ANDREY LOGOSHKO <forando@users.noreply.github.com>
2015-06-25 12:52:11 +0300

    ConditioningConsoleMessage class created

---
+++ src/system/ConditioningConsoleMessage.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2015. This code is a LogosProg property. All Rights Reserved.
+ */
+
+package system;
+
+/**
+ * Created by forando on 25.06.15.<br/>
+ * This class adds some conditions to {@link ConsoleMessage} class functionality.
+ */
+public class ConditioningConsoleMessage {
+
+    private boolean printDebugMessages;
+
+    public ConditioningConsoleMessage(boolean printDebugMessages){
+        this.printDebugMessages = printDebugMessages;
+    }
+
+    /**
+     * Prints debug message only if {@link #printDebugMessages} = TRUE.<br/><br/>
+     * This method wraps an input message with ANSI escape code
+     * that allows to print that message to console in <b>GREEN</b> color.<br>
+     * This formatting doesn't work on Windows console.
+     * @param message The message to be printed to a console.
+     */
+    public void printDebugMessage(String message){
+        if (printDebugMessages)ConsoleMessage.printDebugMessage(message);
+    }
+}

a3f6340d6e647493becbc8f1917ce8e9c149bf74
ANDREY LOGOSHKO <forando@users.noreply.github.com>
2015-12-01 18:07:28 +0200

    OutPut cleaned

--- src/sockets/OutPut.java
+++ src/sockets/OutPut.java
@@ -2,37 +2,23 @@ package sockets;/*
  * Copyright (c) 2014. This code is a LogosProg property. All Rights Reserved.
  */
 
-import java.io.IOException;
 import java.io.ObjectOutputStream;
-import java.net.Socket;
 import java.util.concurrent.Callable;
 
 /**
  * Created by forando on 01.10.14.<br>
- *     This class uses socket to send messages.
+ *     This class uses socket output stream to send messages.
  */
 public class OutPut  implements Callable<Void> {
-    public static final String THREAD_NAME = "SocketOutPut";
-//    private volatile Thread myThread;
     ObjectOutputStream out;
     private int id;
     private Object messageObject;
 
     public OutPut(ObjectOutputStream out, int id, Object messageObject){
-//        this.setName(THREAD_NAME);
-//        myThread = this;
         this.out = out;
         this.id = id;
         this.messageObject = messageObject;
     }
-
-    /*public void stopThread() {
-        Thread tmpThread = myThread;
-        myThread = null;
-        if (tmpThread != null) {
-            tmpThread.interrupt();
-        }
-    }*/
 
     @Override
     public Void call() {
